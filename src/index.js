e5083119debcf14962ef15b6163f8fdf395e40d0
Goldie <goldiejackson25@yahoo.com>
2021-09-08 07:57:44 -0500

    completed - did not complete extra

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const desc = document.getElementById("new-task-description");
+  const form = document.getElementById("create-task-form")
+  
+  function removeInput (e) { 
+  const tasksList = document.getElementById("tasks"); 
+  tasksList.appendChild(e);
+  e.preventDefault();
+  }
+
+  form.addEventListener('submit', removeInput);
 });
